be5bfd28ab6dc0edf68ed7fa864814c113cdcc53
Alexander Scheibler <60936422+alexanderscheibler@users.noreply.github.com>
2024-11-01 16:21:41 -0300

    feat: add URL for sample testing

--- test/basicConfig.js
+++ test/basicConfig.js
@@ -1,15 +1,15 @@
-const baseUrl = "http://example.com"; // Replace the value "http://example.com" by the base URL of the website you want to test.
+const baseUrl = "https://the-internet.herokuapp.com/" // by the base URL of the website you want to test.
 const projectId = "sample project"; // Replace the value "sample project" by the id of your project. It can be any string (e.g., "my-website").
 
 // Replace the values of the below array with the relative URLs of your website. E.g., "/about", "/contact", "/pricing", etc.
 // Use just "/" to test the homepage of your website.
 // Add as many relative URLs as you need.
 const relativeUrls =[
-  "/",
-  "/page1",
-  "/page2",
-  "/page3",
-  "/page4",
+  "/abtest",
+  "/disappearing_elements",
+  "/tables",
+  "/large",
+  "/dynamic_content"
 ];
 
 // Leave the below array as is if you want to test your website using the viewports listed below.
